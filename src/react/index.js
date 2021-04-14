9e71b8795cb48f73fc6675223e566e3bccd7ebe2
徐文浩 <934367068@qq.com>
2021-04-14 09:32:13 +0800

    [ADD]函数组件类组件，hooks的实现

--- src/react/index.js
+++ src/react/index.js
@@ -1,6 +1,7 @@
 
-
-import { ELEMENT_TEXT } from './constants';
+import { UpdateQueue, Update } from './UpdateQueue';
+import { ELEMENT_TEXT, UPDATE } from './constants';
+import { scheduleRoot, useReducer, useState } from '../react-dom/scheduler';
 function createElement(type, config = {}, ...children) {
     return {
         type,
@@ -18,8 +19,26 @@ function createElement(type, config = {}, ...children) {
     }
 }
 
-const React={
-    createElement   
+class Component {
+    constructor(props) {
+        this.props = props;
+
+    }
+
+    setState(payload) {//可能是对象,也可能是函数
+        let update = new Update(payload);
+        this.internalFiber.updateQueue.enqueueUpdate(update);
+        scheduleRoot()
+
+    }
+
+}
+
+Component.prototype.isReactComponent = {};//类组件
+const React = {
+    createElement,
+    Component,
+    useReducer, useState
 }
 
 export default React;
